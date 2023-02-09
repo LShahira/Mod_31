1684e71cedfdb3f26d2993a1131f40048e25c0b5
LShahira <shahiralabeeba@gmail.com>
2023-02-09 13:10:33 -0600

    js recap

--- ES6-intro/basic.js
+++ ES6-intro/basic.js
@@ -11,4 +11,15 @@ else if (deposit==500){
 else{
 
 }
-// != not equals, == equals, <= lesser or equal, >= gretaer or equal, < less, > greater
+// != not equals, == equals, <= lesser or equal, >= greater or equal, < less, > greater
+
+//array
+const numbers=[45,6548,124, 45,2,365];
+const numberCount= number.length //how many numbers
+numbers.pop();
+numbers.push(35); // pushesh a number in the array
+numbers[2]=69; // put 69 in second index
+
+if (numbers.indexOf(222)!=-1){ /// check whether  222 in the array
+
+}
