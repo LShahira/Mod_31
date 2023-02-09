940b6e94a426da285a3cd76469494230d34a1c7e
LShahira <shahiralabeeba@gmail.com>
2023-02-09 12:20:12 -0600

    ES6 intro

---
+++ ES6-intro/basic.js
@@ -0,0 +1,14 @@
+//variable
+var deposit = 400;
+
+//condition
+if(deposit>500){
+
+}
+else if (deposit==500){
+
+}
+else{
+
+}
+// != not equals, == equals, <= lesser or equal, >= gretaer or equal, < less, > greater
